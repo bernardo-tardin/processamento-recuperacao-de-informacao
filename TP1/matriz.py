b017cd35317606fd1f81ba070d28c6a1dc72ac48
Bernardo Moraes <147925451+bernardo-tardin@users.noreply.github.com>
2024-02-26 10:42:17 +0000

    Adição do módulo que permite queries booleanas

--- TP1/matriz.py
+++ TP1/matriz.py
@@ -16,7 +16,7 @@ def tokens(file):
     tokens = list(set(tokens))
     return tokens
 
-def main(file1,file2,file3):
+def main(file1,file2,file3,query):
     dicionario = {"medicina":[0,0,0],"medieval":[0,0,0],"europa":[0,0,0],"igreja":[0,0,0],"literatura":[0,0,0],"filosofia":[0,0,0],
                   "livro":[0,0,0],"obra":[0,0,0],"cura":[0,0,0]}
     tokens1 = tokens(file1)
@@ -39,6 +39,31 @@ def main(file1,file2,file3):
             for i in range(len(key),10):
                 termo += " "
         print(f"  {termo}         {dicionario[key][0]}               {dicionario[key][1]}            {dicionario[key][2]}")
-    return dicionario
+    dic_decimal={}
+    for key in dicionario:
+        binary_num = ''
+        for bit in dicionario[key]:
+            binary_num += str(bit)
+        decimal_num = int(binary_num,2)
+        dic_decimal[key] = decimal_num
+    query = query.replace("AND","&")
+    query = query.replace("OR","|")
+    query = query.replace("NOT","~")
+    termos = query.split()
+    for i in range(len(termos)):
+        if termos[i] not in ["&", "|", "~", "(", ")"]:
+            termos[i] = str(dic_decimal[termos[i]])
+    new_query = ' '.join(termos)
+    n = eval(new_query)
+    binary = str(bin(n))
+    if len(binary) < 5:
+        binary = binary.replace("b","")
+    else:
+        binary = binary.replace("0b","")
+    docs = []
+    for i in range(0,len(binary)):
+        if binary[i] == '1':
+            docs.append("hilda"+str(i+1)+".pdf")
+    print(docs)
 
-main("hilda1.pdf","hilda2.pdf","hilda3.pdf")
+main("hilda1.pdf","hilda2.pdf","hilda3.pdf","filosofia AND ( livro OR obra ) AND europa")
